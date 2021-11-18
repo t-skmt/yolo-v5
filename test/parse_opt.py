5b98e0ba0af49dedc4b12baa54e8107655308413
t-skmt <skmt1224@gmail.com>
2021-11-18 09:48:45 +0000

    add argument

--- test/parse_opt.py
+++ test/parse_opt.py
@@ -22,8 +22,26 @@ parser.add_argument("--rect", action="store_true", help="rectangular training")
 parser.add_argument("--resume", nargs="?", const=True, default=False, help="resume most resent training")
 # --resumeの後ろに引数が1つあるならそれを、なければconstの値(=True)を使用
 parser.add_argument("--nosave", action="store_true", help="only save final checkpoint")
+parser.add_argument("--noval", action="store_true", help="only validate final epoch")
+parser.add_argument("--noautoanchor", action="store_true", help="disable autoanchor check")
+parser.add_argument("--evolve", type=int, nargs="?", const=300, help="evolve hyperparameters for x generations")
+#  --evolve: ハイパーパラメータの最適化手法に遺伝的アルゴリズムを使用する
+parser.add_argument("--bucket", type=str, default="", help="gsutil bucket")
+# evolve.csvをダウンロードするためのGoogle Cloud Storageのバケットを指定。--evolveを使用するときのみ有効。
+parser.add_argument("--cache", type=str, nargs="?", const="ram", help="--cache images in 'ram' (default) or 'disk'")
+parser.add_argument("--image-weights", action="store_true", help="use weighted image selection for training")
+# 前のエポックでのmAPの逆数で重み付けされた画像をサンプリングする
+parser.add_argument("--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu")
+parser.add_argument("--multi-scale", action="store_true", help="vary img-size +/-50%%")
+# 学習中に画像サイズを+/-50%変化させる
+parser.add_argument("--single-cls", action="store_true", help="train multi-class data as single-class")
+# すべてのクラスを1クラス("item")として学習させる
+parser.add_argument("--adam", action="store_true", help="use torch.optim.Adam() optimizer")
+parser.add_argument("--sync-bn", action="store_true", help="use SyncBatchNorm, only available in DDP mode")
+
+
 
 opt = parser.parse_known_args()[0] if known else parser.parse_args()
 
 print(opt)
-print(opt.resume)
+print(opt.multi_scale)
