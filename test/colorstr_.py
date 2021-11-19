066b3824e53cf5ac7ea7ac291ada502be66672ae
t-skmt <skmt1224@gmail.com>
2021-11-19 08:23:16 +0000

    Create colorstr_.py

---
+++ test/colorstr_.py
@@ -0,0 +1,41 @@
+
+def colorstr(*input):
+    print(len(input))  # 2
+    # *args, string = input
+    # print(args)  # ['blue'] if len(input) == 0 → []
+    # print(string)  # hello world
+
+    *args, string = input if len(input) > 1 else ("blue", "bold", input[0])
+    print(args)  # ['blue', 'bold']
+    print(string)  # one
+    colors = {'black': '\033[30m',  # basic colors
+              'red': '\033[31m',
+              'green': '\033[32m',
+              'yellow': '\033[33m',
+              'blue': '\033[34m',
+              'magenta': '\033[35m',
+              'cyan': '\033[36m',
+              'white': '\033[37m',
+              'bright_black': '\033[90m',  # bright colors
+              'bright_red': '\033[91m',
+              'bright_green': '\033[92m',
+              'bright_yellow': '\033[93m',
+              'bright_blue': '\033[94m',
+              'bright_magenta': '\033[95m',
+              'bright_cyan': '\033[96m',
+              'bright_white': '\033[97m',
+              'end': '\033[0m',  # misc
+              'bold': '\033[1m',
+              'underline': '\033[4m'}
+    print("---")
+    for x in args:
+        print(x)
+
+    print("".join(colors[x] for x in args) + f"{string}" + colors['end'])
+
+    return
+
+
+if __name__ == "__main__":
+    colorstr("red", "hello world")
+    # colorstr("one")
