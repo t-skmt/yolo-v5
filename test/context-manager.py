e6596f2cd8e6b8ad2c0eac29e0235f80943fef92
t-skmt <skmt1224@gmail.com>
2021-12-03 05:11:19 +0000

    Update context-manager.py

--- test/context-manager.py
+++ test/context-manager.py
@@ -1,49 +1,40 @@
-class MyManager():
+import contextlib
+from pathlib import Path
+import os
+
+class WorkingDirectory(contextlib.ContextDecorator):
+    """一時的に作業ディレクトリを移動する"""
+    def __init__(self, new_dir):
+        self.dir = new_dir
+        self.cwd = Path.cwd().resolve()
+        pass
+
     def __enter__(self):
-        print("enter☟")
-        print(self.__dict__)
+        print("enter")
+        print("self.dir:", self.dir)
+        print("self.cwd:", self.cwd)
+        print("now:", os.getcwd())
+        os.chdir(self.dir)
+        print("now:", os.getcwd())
     
-    def __exit__(self):
-        print("exit ☞")
+    def __exit__(self, exc_type, exc_value, trace):
+        print("exit")
+        print("now:", os.getcwd())
+        os.chdir(self.cwd)
+        print("now:", os.getcwd())
+        print('Exception type: {}'.format(exc_type))
+        print('Exception value: {}'.format(exc_value))
+        print('Exception trace back {}'.format(trace))
 
-m = MyManager()
-with m:
-    print(1 + 1)
-
-# import contextlib
-# from pathlib import Path
-# import os
-
-# FILE = Path(__file__).resolve()
-# ROOT = FILE.parents[1]
-# print(ROOT)
-# print(type(ROOT))
-
-# class WorkingDirectory(contextlib.ContextDecorator):
-#     def __init__(self, newdir):
-#         self.dir = newdir
-#         self.cwd = Path.cwd().resolve()
-    
-#     def __enter__(self):
-#         print("enter☟")
-#         os.chdir(self.dir)
-
-#     def __exit__(self, exc_type, exc_val, exc_tb):
-#         print(exc_type, exc_val, exc_tb)
-#         print("exit ☞")
-#         print(self.cwd)
-#         os.chdir(self.cwd)
-
-# print(Path.cwd().resolve())
-
-# @WorkingDirectory(ROOT)
-# def sayhello():
-#     print("Hello,")
-#     print(os.getcwd())
-#     print("ROOT -> ")
-#     a, b, c, d = ROOT
-#     print(a, b, c, d)  #TypeError: cannot unpack non-iterable PosixPath object
+@WorkingDirectory("/workspace")  # with文でも使用可能
+def show_items():
+    import subprocess
+    print("-----")
+    subprocess.run(["ls"])
+    print("-----")
 
 
-# if __name__ == "__main__":
-#     sayhello()
+if __name__ == "__main__":
+    # with WorkingDirectory("/workspace"):  # decoratorでも使用可能
+    #     show_items()
+    show_items()
